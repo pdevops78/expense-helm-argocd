0d99038141ca925dc07ef557a853941fa696632c
Your Name <vemasani.pavani@gmail.com>
2025-08-26 12:53:12 +0530

    helm NGINX ingress

--- argocd.sh
+++ argocd.sh
@@ -12,7 +12,7 @@ if [ $1 == "install" ]; then
 fi
 #  to add jobs in argocd
   if [ $1 == "jobs" ]; then
-   argocd login ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1} --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+   argocd login $(kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
    for app in frontend backend; do
    argocd app create ${app} --repo https://github.com/pdevops78/expense-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default --insecure  --grpc-web --values values/${app}.yaml
   done
